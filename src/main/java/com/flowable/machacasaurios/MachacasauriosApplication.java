c12b1f89364bcba104a3daf2a93fa893b07806d9
inaki.huete <inaki.huete@flowable.com>
2020-02-20 18:59:36 +0100

    puert

--- src/main/java/com/flowable/machacasaurios/MachacasauriosApplication.java
+++ src/main/java/com/flowable/machacasaurios/MachacasauriosApplication.java
@@ -20,11 +20,6 @@ import org.springframework.core.io.ClassPathResource;
 @SpringBootApplication
 public class MachacasauriosApplication implements CommandLineRunner {
 
-    //    public static final String PATH = "a_example.txt";
-    //        public static final String PATH = "b_lovely_landscapes.txt";
-    //        public static final String PATH = "c_memorable_moments.txt";
-    //        public static final String PATH = "d_pet_pictures.txt";
-    //        public static final String PATH = "e_shiny_selfies.txt";
 
     public static List<String> inputFiles = Arrays.asList("a_example.txt",
 //        "b_lovely_landscapes.txt"
@@ -36,8 +31,6 @@ public class MachacasauriosApplication implements CommandLineRunner {
 
 //    public static List<String> inputFiles = Arrays.asList("a_example.txt");
 
-    static long numberOfPhotos;
-    static List<Photo> photos = new ArrayList<>();
 
     private static Logger LOG = LoggerFactory
         .getLogger(MachacasauriosApplication.class);
@@ -67,47 +60,47 @@ public class MachacasauriosApplication implements CommandLineRunner {
         LOG.info("APPLICATION FINISHED");
     }
 
-    private static void solutionToFile(List<Slide> slides, String fileName) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/" + "solution_" + fileName));
-
-        writer.write(String.valueOf(slides.size()));
-        slides.forEach(slide -> {
-            try {
-                writer.write("\n");
-                writer.write((slide.toString()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        writer.close();
-
-    }
+//    private static void solutionToFile(List<Slide> slides, String fileName) throws IOException {
+//        BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/" + "solution_" + fileName));
+//
+//        writer.write(String.valueOf(slides.size()));
+//        slides.forEach(slide -> {
+//            try {
+//                writer.write("\n");
+////                writer.write((slide.toString()));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        writer.close();
+//
+//    }
     private static void readFromInputStream(InputStream inputStream)
         throws IOException {
         StringBuilder resultStringBuilder = new StringBuilder();
-        try (BufferedReader br
-                 = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            int i = 0;
-            while ((line = br.readLine()) != null) {
-                if (i == 0) {
-                    numberOfPhotos = Long.parseLong(line);
-                } else {
-                    String[] splited = line.split("\\s+");
-                    String id = String.valueOf((i - 1));
-                    String numberOfTags = splited[1];
-                    String orientation = splited[0];
-                    List<String> tags = new ArrayList<>();
-
-                    for (int j = 2; j < splited.length; j++) {
-                        tags.add(splited[j]);
-                    }
-                    photos.add(new Photo(id, numberOfTags, orientation, tags));
-                }
-                i++;
-
-            }
-        }
+//        try (BufferedReader br
+//                 = new BufferedReader(new InputStreamReader(inputStream))) {
+//            String line;
+//            int i = 0;
+//            while ((line = br.readLine()) != null) {
+//                if (i == 0) {
+//                    numberOfPhotos = Long.parseLong(line);
+//                } else {
+//                    String[] splited = line.split("\\s+");
+//                    String id = String.valueOf((i - 1));
+//                    String numberOfTags = splited[1];
+//                    String orientation = splited[0];
+//                    List<String> tags = new ArrayList<>();
+//
+//                    for (int j = 2; j < splited.length; j++) {
+//                        tags.add(splited[j]);
+//                    }
+//                    photos.add(new Photo(id, numberOfTags, orientation, tags));
+//                }
+//                i++;
+//
+//            }
+//        }
     }
     
     @Override
